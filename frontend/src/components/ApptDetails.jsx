2f00938da2bf8f2a2060cda61101c829f61c66f0
LianLambert <lian1lambert@gmail.com>
2024-12-02 11:11:50 -0500

    feat: add ApptDetails component

---
+++ frontend/src/components/ApptDetails.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import '../index.css';
+import '../css/appt-details.css';
+import { FaRegClock } from "react-icons/fa";
+import { CgAttachment } from "react-icons/cg";
+import { MdEdit } from "react-icons/md";
+
+/* Example usage from App.jsx:
+
+  import profilePic from './assets/testProfilePic.png';
+
+  const fakeAppt = {
+    id: '1234',
+    hostName: 'Joseph Vybihal',
+    hostProfilePic: profilePic,
+    title: 'Professor Vybihal\'s Office Hours',
+    duration: 30,
+    location: 'In person, Leacock 910',
+    description: 'Please come prepared with your project outline filled out. See attachment for required components.',
+    attachment: 'COMP_307_project_outline.pdf',
+  };
+
+  <ApptDetails appt={fakeAppt} showEditButton={true} /> 
+*/
+
+/* TO DO:
+  - connect backend (will likely need to make changes depending on our entity format)
+  - after backend is set up, links should open actual attachments
+  - make sure duration is stored in mins or else make changes
+*/
+
+
+const ApptDetails = ({ appt, showEditButton=false }) => {
+  const { id, hostName, hostProfilePic, title, duration, location, description, attachment } = appt;
+
+  return (
+    <div id='appt-details-container'>
+      <div id='profile-container'>
+        <img
+          src={hostProfilePic}
+          class='profile-circle'
+          style={{ marginRight: '10px' }}
+        />
+        {hostName}
+      </div>
+      <h1>{title}</h1>
+      <div id='appt-time-container' class='row'>
+        <FaRegClock id='clock-icon'/>
+        {duration} mins
+      </div>
+      {location && (
+        <div class='row'>
+          <span class='bold-text'>Location: </span>
+          <span>{location}</span>
+        </div>
+      )}
+      {description && (
+        <div class='row'>
+         <span class='bold-text'>Description: </span>
+         <span>{description}</span>
+        </div>
+      )}
+      {attachment && (
+        <div id='attachment-container' class='row'>
+          <CgAttachment class='icon'/>
+          <span class='bold-text'>Attachment: </span>
+          <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley' target="_blank" rel="noopener noreferrer">
+            {attachment}
+          </a>
+        </div>
+      )}
+      {showEditButton && (
+        <div class='row centered'>
+          <Link 
+            to={`/edit?id=${id}`}
+            id='edit-button' 
+            className='border-base centered'
+          >
+            <MdEdit className='icon' />
+            Edit
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ApptDetails;
--- frontend/src/pages/EditPage.jsx
+++ frontend/src/pages/EditPage.jsx
@@ -1,10 +1,18 @@
 import NavBar from "../components/NavBar"
+import { useLocation } from 'react-router-dom';
 
 function EditPage() {
+
+  // to retrieve appt id if editing an already existing appt
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const apptId = queryParams.get('id');
+
   return (
     <>
       <NavBar />
       <h1>This is the page to create and edit pages</h1>
+      <div>Appt Id (for testing): {apptId || 'creating a new meeting, no id yet'}</div>
     </>
   )
 }
