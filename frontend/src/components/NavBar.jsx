61eb8458e19f39ffb9e79bfe748545ee7ddeda18
Vincent St-Pierre <vstpierre1@gmail.com>
2024-12-05 01:48:19 -0500

    fix: notification panel styling and behaviour

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,11 +5,13 @@ import { useState, useEffect } from 'react'
 
 // Icon Imports
 import { FiLogIn } from 'react-icons/fi'
-import { IoIosNotifications } from 'react-icons/io'
+import { VscBell } from "react-icons/vsc";
+import { VscBellDot } from "react-icons/vsc";
 import { IoPersonAdd } from 'react-icons/io5'
 import { FaPen } from 'react-icons/fa'
 import { IoPeople } from 'react-icons/io5'
 import { HiDocument } from 'react-icons/hi2'
+import { BsExclamation } from "react-icons/bs";
 
 function NavBar() {
   // Login Status
@@ -20,9 +22,22 @@ function NavBar() {
   // Notification Panel Toggle (Bell Icon)
   const [isNotifsOpen, setIsNotifsOpen] = useState(false)
 
+  // Notification Bell with new notifications
+  const [isNewNotifs, setIsNewNotifs] = useState(true)
+
   // Function for toggling notifications
   const toggleNotifs = () => {
     setIsNotifsOpen((prev) => !prev) // Flipping state
+    
+    // Removing new notification dot if applicable
+    if(isNewNotifs){
+      toggleNewNotifs()
+    }
+  }
+
+  // Function for toggling new notification symbol
+  const toggleNewNotifs = () => {
+    setIsNewNotifs((prev) => !prev) // Flipping state
   }
 
   return (
@@ -48,7 +63,10 @@ function NavBar() {
               Documents
             </Link>
 
-            <IoIosNotifications id="bell-icon" onClick={toggleNotifs} />
+            {/* Notification Icon with dot for new notifications*/}
+            {isNewNotifs ? (<VscBellDot class="bell-icon" onClick={toggleNotifs} />
+            ) : (<VscBell class="bell-icon" onClick={toggleNotifs} />)
+            }
 
             <Link to="/" class="nav-button">
               <FiLogIn className="icon1" />
@@ -57,14 +75,63 @@ function NavBar() {
 
             {/* Notification Panel Popup Test with List*/}
             {isNotifsOpen && (
-              <div id="notifs-panel">
+              <div id="notifs-panel" onMouseLeave={toggleNotifs}>
                 <button id="close-notifs" onClick={toggleNotifs}> &times; </button>
-                <ul>
-                  <li> Professor Vyhibal’s Office Hours: A document was attached. </li>
-                  <li> Professor Vyhibal’s Office Hours: Meeting was modified. </li>
-                  <li> Professor Vyhibal’s Office Hours: You were invited. </li>
-                  <li> Welcome to Commit2Gather! </li>
-                </ul>
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+
+                <div class="notifs-container">
+                  <div class="notifs-top-bar">
+                    <HiDocument class="notifs-logo" />
+                    <a href="#" class="notifs-title"><b><u>Professor's Vyhibal Office Hours</u></b></a>
+                  </div>
+                  <p class="notifs-desc">A document was attached to this meeting you are attending.</p>
+                  <p class="notifs-desc">Sunday, November 24, 2024, 8:19pm, by Joseph Vyhibal</p>
+                </div>
+              
+                
               </div>
             )}
           </div>
