f17c488ab9c06009754ca570b2140c14ae4c1415
Lilan <lilan0213@yahoo.com>
2024-12-12 16:27:44 -0500

    feat: create edit calendar ui on create page

--- frontend/src/pages/EditPage.jsx
+++ frontend/src/pages/EditPage.jsx
@@ -1,18 +1,62 @@
-import NavBar from "../components/NavBar"
-import { useLocation } from 'react-router-dom';
+import Calendar from '../components/Calendar'
+import NavBar from '../components/NavBar'
+import EventDetails from '../components/EventDetails'
+import ScheduleSettings from '../components/ScheduleSettings'
+import { useLocation } from 'react-router-dom'
+import { act, useState } from 'react'
+import '../css/edit-page.css'
 
-function EditPage() {
+const EditPage = () => {
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const apptId = queryParams.get('id')
 
-  // to retrieve appt id if editing an already existing appt
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const apptId = queryParams.get('id');
+  // Tabs
+  const [activeTab, setActiveTab] = useState('EventDetails')
+
+  const renderTabs = () => {
+    if (activeTab === 'EventDetails') {
+      return <EventDetails />
+    } else if (activeTab === 'ScheduleSettings') {
+      return <ScheduleSettings />
+    }
+    return null
+  }
 
   return (
     <>
       <NavBar />
-      <h1>This is the page to create and edit pages</h1>
-      <div>Appt Id (for testing): {apptId || 'creating a new meeting, no id yet'}</div>
+      {/* Edit Calendar Side */}
+      <div className="create-page-wrapper">
+        <div className="edit-wrapper">
+          <h2>{apptId ? 'Edit Event' : 'Create New Event'}</h2>
+
+          {/* Navigation Tabs */}
+          <div className="tab-navigation"></div>
+          <div className="tab-navigation">
+            <button
+              className={activeTab === 'EventDetails' ? 'active-tab' : ''}
+              onClick={() => setActiveTab('EventDetails')}
+            >
+              Event Details
+            </button>
+            <button
+              className={activeTab === 'ScheduleSettings' ? 'active-tab' : ''}
+              onClick={() => setActiveTab('ScheduleSettings')}
+            >
+              Schedule Settings
+            </button>
+          </div>
+
+          {/* Tab Content */}
+          <div className="tab-content">{renderTabs()}</div>
+        </div>
+
+        {/* Preview Calendar Side */}
+        <div className="preview-wrapper">
+          <Calendar />
+        </div>
+      </div>
     </>
   )
 }
