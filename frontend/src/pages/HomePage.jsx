e0e9ffbdcdeacfc9ece6e493b062d100fdd23473
Vincent St-Pierre <vstpierre1@gmail.com>
2024-12-15 16:21:10 -0500

    feat: Homepage and landingpage styling

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,12 +2,19 @@ import React, {useState, useEffect} from 'react';
 import NavBar from '../components/NavBar';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import '../css/home-page.css';
+import marty from '../assets/marty_happy 09.23.00 1.png';
 
 function HomePage() {
 
   const [firstName, setFirstName] = useState('');
   const location = useLocation();
   const { id } = location.state || {}; 
+
+  // Dropdown text to describe navbar utilities!
+  const [showCreate, setShowCreate] = useState(false);
+  const [showMeetings, setShowMeetings] = useState(false);
+  const [showDocuments, setShowDocuments] = useState(false);
   
   // We want to show user first name after first render
   useEffect(() => { // Let's avoid giving access to user data directly.
@@ -25,8 +32,38 @@ function HomePage() {
   return (
     <>
       <NavBar userId={id} />
-      <h1>Hello {firstName}!</h1>
-      <h1>This is the Home Page After Users Login</h1>
+      <div className="home-page">
+        <div className="home-page-content">
+          <div className="hello-box">
+            <div className="hello-text">
+              <h1>Hello {firstName}!</h1>
+              <p>Welcome back to Commit2Gather! Let's get your meetings setup! See below a list of the utilities accessible to you: </p>
+            </div>
+            <div className="marty-image-container">
+              <img className="marty-image" src={marty}/>
+            </div>
+          </div>
+
+          {/* Dropdowns are clicked on and off and change the display of the text on click*/}
+          <div className="dropdown-box">
+            <div className="dropdown-header" onClick={() => setShowCreate(!showCreate)}>
+              <h2>Create</h2>
+              {showCreate && <p style={{ display: showCreate ? "block" : "none" }}>
+                Create an event for your meeting with you as the host. Edit details such as duration, location, and reccurence of the meeting over time.</p>}
+            </div>
+            <div className="dropdown-header" onClick={() => setShowMeetings(!showMeetings)}>
+              <h2>Meetings</h2>
+              {showMeetings && <p style={{ display: showMeetings ? "block" : "none" }}>
+                All the informations for your meetings is stored in this table. Contains all meetings your participate in, but also all participation requests you received.</p>}
+            </div>
+            <div className="dropdown-header" onClick={() => setShowDocuments(!showDocuments)}>
+              <h2>Documents</h2>
+              {showDocuments && <p style={{ display: showDocuments ? "block" : "none" }}>
+                Take a look at all the document attachments from relevant meetings you participate in.</p>}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from 'react';
+import NavBar from '../components/NavBar';
+import '../css/landing-page.css';
+import marty from '../assets/marty_happy 09.23.00 1.png';
+import preview from '../assets/Landing Page Website Preview.jpg'
+
 
 function LandingPage() {
   return (
     <>
       <NavBar />
-      <h1>This is the Landing Page After Users Logout</h1>
+      
+      <div className='landing-page'>
+        <div className='landing-page-content'>
+          <div className='landing-page-text-box'>
+            <div className='text-box-left'>
+              <h1 className='text-box-title'>Welcome to Commit<span className="red-text">2</span>Gather!</h1>
+              <p className='text-box-content'>This booking tool allows McGill University students and teachers alike to better manage their meetings/appointments.
+                  Commit2Gather uses a visually intuitive calendar view to keep track of your booking schedule.
+                  Users can create or edit their own meetings, and then notify invitees. All future & past meetings can be seen on the Meetings page!</p>
+            </div>
+            <div className='text-box-right'>
+              <img className='marty-image' src={marty}></img>
+            </div>        
+          </div>
+          <img className='preview-image' src={preview}></img>
+        </div>
+      </div>
     </>
   )
 }
