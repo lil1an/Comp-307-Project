759c6606d606b000aa0d4b85f3512c6d42ee21e0
LianLambert <lian1lambert@gmail.com>
2024-12-15 08:03:34 -0500

    fix: schemas reference collections not models

--- backend/models/Meeting.js
+++ backend/models/Meeting.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const meeting = new Schema({
-  host: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  host: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
   title: { type: String, required: true },
   description: { type: String, required: false },
   availabilities: { type: Map, of: Array, required: true }, // Will store {key : [start: time, end: time]}
