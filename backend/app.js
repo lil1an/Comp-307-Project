565367b38f9064e36892d076db7af75550ec8d57
Lilan <lilan0213@yahoo.com>
2024-12-17 15:13:09 -0500

    feat: serve frontend static files from backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-// Import environment variables
+import path from 'path' // Import path for serving static files
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -6,28 +6,28 @@ dotenv.config()
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import http from 'http';
-import { initializeSocket } from './socket.js';
-import connectDB from './config/database.js';
-import userRoutes from './routes/userRoutes.js';
-import meetingRoutes from './routes/meetingRoutes.js';
-import requestRoutes from './routes/requestRoutes.js';
-import notificationRoutes from './routes/notificationRoutes.js';
+import http from 'http'
+import { initializeSocket } from './socket.js'
+import connectDB from './config/database.js'
+import userRoutes from './routes/userRoutes.js'
+import meetingRoutes from './routes/meetingRoutes.js'
+import requestRoutes from './routes/requestRoutes.js'
+import notificationRoutes from './routes/notificationRoutes.js'
 
 // Import models
 import User from './models/User.js'
 
 // Connect to the database
-await connectDB();
+await connectDB()
 
 // Initialize express app
-const app = express();
+const app = express()
 
 // Initialize Server
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 // Socket.io functionalities
-initializeSocket(server);
+initializeSocket(server)
 
 // middleware
 app.use(morgan('dev'))
@@ -36,17 +36,24 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors({ origin: true, credentials: true }))
 
 // Routes
-app.use('/users', userRoutes);
-app.use('/meetings', meetingRoutes);
-app.use('/requests', requestRoutes);
-app.use('/notifications', notificationRoutes);
+app.use('/users', userRoutes)
+app.use('/meetings', meetingRoutes)
+app.use('/requests', requestRoutes)
+app.use('/notifications', notificationRoutes)
+
+// Serve Frontend Static Build Files
+const __dirname = path.resolve() // Set __dirname for ES module compatibility
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
 
-
-
-export default app;
+export default app
